8a2c43fa88743ff2c9e317cf9564aaec106ee2da
Rodrigo Carvalho <carvalhorr@gmail.com>
2017-03-14 17:07:13 +0000

    Created blank tests to check schema creation and data insertion on DB.

---
+++ src/main/tests/carvalhorr/cs654/persistence/OshDataPersistenceTests.java
@@ -0,0 +1,32 @@
+package carvalhorr.cs654.persistence;
+
+import org.junit.Test;
+
+public class OshDataPersistenceTests {
+
+	
+	@Test
+	public void insertUserSuccessfull() {
+		
+	}
+	
+	@Test
+	public void insertNodeSuccessfull() {
+		
+	}
+	
+	@Test
+	public void isnertWaySuccessfull() {
+		
+	}
+	
+	@Test
+	public void insertTagsSuccessfull() {
+		
+	}
+	
+	@Test
+	public void computeWayCoordinatesSuccessfull() {
+		
+	}
+}
---
+++ src/main/tests/carvalhorr/cs654/persistence/OshSchemaCreationPersistenceTests.java
@@ -0,0 +1,24 @@
+package carvalhorr.cs654.persistence;
+
+import org.junit.Test;
+
+/**
+ * Tests to verify that a schema is created correctly and that data is deleted
+ * when creating an schema whose name already exists.
+ * 
+ * @author carvalhorr
+ *
+ */
+public class OshSchemaCreationPersistenceTests {
+
+	@Test
+	public void createSchemaTest() {
+
+	}
+
+	@Test
+	public void createExistingSchemaDeletesPreviousDataTest() {
+
+	}
+
+}
