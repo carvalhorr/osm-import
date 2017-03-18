1b0174c2fb688a4092bd44b3634ba0dbb9c9c3ea
Rodrigo Carvalho <carvalhorr@gmail.com>
2017-03-18 21:02:56 +0000

    Tests to verify all tables with correct structure are created.

--- src/main/tests/carvalhorr/cs654/persistence/OshTestsPersistence.java
+++ src/main/tests/carvalhorr/cs654/persistence/OshTestsPersistence.java
@@ -6,13 +6,25 @@ import java.sql.Statement;
 import carvalhorr.cs654.exception.ErrorConnectingToDatabase;
 import carvalhorr.cs654.exception.PostgresqlDriverNotFound;
 
+/**
+ * Persistence class to allow database related tests.
+ * 
+ * @author carvalhorr
+ *
+ */
 public class OshTestsPersistence extends OshDatabasePersistence {
 
 	public OshTestsPersistence(String jdbcString, String user, String password, String schemaName)
 			throws SQLException, PostgresqlDriverNotFound, ErrorConnectingToDatabase {
 		super(jdbcString, user, password, schemaName);
 	}
-	
+
+	/**
+	 * Exposes the statement object from the persistence in order for custom
+	 * queries to be executed.
+	 * 
+	 * @return
+	 */
 	public Statement getStatement() {
 		return statement;
 	}
