27729ed979862ecefcac58827c25f87ac2b8a5a7
Rodrigo Carvalho <carvalhorr@gmail.com>
2017-03-20 18:11:39 +0000

    Tests for the OSH string processing

--- src/main/java/carvalhorr/cs654/model/WayOsmObject.java
+++ src/main/java/carvalhorr/cs654/model/WayOsmObject.java
@@ -1,6 +1,5 @@
 package carvalhorr.cs654.model;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,8 +8,6 @@ import carvalhorr.cs654.osh.WayCoordinatesProcessor;
 public class WayOsmObject extends OsmObject {
 
 	WayCoordinatesProcessor wayCoordinates = new WayCoordinatesProcessor();
-
-	List<String> nodes = new ArrayList<String>();
 
 	public void addNode(String nodeId) {
 		wayCoordinates.addNode(nodeId);
---
+++ src/main/tests/carvalhorr/cs654/osh/PropertiesExtractorTests.java
@@ -0,0 +1,34 @@
+package carvalhorr.cs654.osh;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PropertiesExtractorTests {
+
+	@Test
+	public void extractPropertiesFromOsmStringSuccessfull() {
+		
+		String osmString = "<node id=\"4221833\" version=\"2\" timestamp=\"2008-12-11T14:21:24Z\" uid=\"15535\" user=\"Blazejos\" changeset=\"315455\" visible=\"true\" lat=\"53.3826303\" lon=\"-6.5995123\"/>";
+		
+		Map<String, String> properties = PropertiesExtractor.extractPropertiesFromLine(osmString);
+
+		checkProperty(properties, "id","4221833");
+		checkProperty(properties, "version","2");
+		checkProperty(properties, "timestamp", "2008-12-11T14:21:24Z");
+		checkProperty(properties, "uid", "15535");
+		checkProperty(properties, "user", "Blazejos");
+		checkProperty(properties, "changeset", "315455");
+		checkProperty(properties, "visible", "true");
+		checkProperty(properties, "lat", "53.3826303");
+		checkProperty(properties, "lon", "-6.5995123");
+
+	}
+	
+	private void checkProperty(Map<String, String> properties, String key, String value) {
+		assertTrue(properties.containsKey(key));
+		assertEquals(properties.get(key), value);	
+	}
+}
